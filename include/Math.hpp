2ca2e3ca2abf904b40184b16ad54aa3a44bdd759
shahbaz-dev <shahbaz-dev@shahbaz.logiciel>
2024-11-04 04:12:44 +0500

    math library complted

---
+++ include/Math.hpp
@@ -0,0 +1,6 @@
+namespace subspace1 {
+class Math {
+ public:
+  static void CalculateSquare(int number);
+};
+}  // namespace subspace1
---
+++ src/Math.cpp
@@ -0,0 +1,9 @@
+#include <Math.hpp>
+#include <iostream>
+
+namespace subspace1 {
+void Math::CalculateSquare(int number) {
+  std::cout << "SP1: " << "Square root of " << number
+            << " is: " << number * number << std::endl;
+}
+}  // namespace subspace1
